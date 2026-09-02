b4b8ac3daf191ec58ffa98c1a7781641547c985b
aaaa24 <chernov.arseniy@yandex.ru>
2026-09-02 22:15:48 +0300

    add insert_event

--- main.py
+++ main.py
@@ -9,24 +9,29 @@ dotenv.load_dotenv()
 class GoogleCalendar:
     SCOPES = ['https://www.googleapis.com/auth/calendar']
 
-    def __init__(self, credentials_filename):
+    def __init__(self, credentials_filename, calendar_id):
         credentials = service_account.Credentials.from_service_account_file(
             filename=credentials_filename, scopes=self.SCOPES
         )
         self.service = build('calendar', 'v3', credentials=credentials)
+        self.calendar_id = calendar_id
+
+    def insert_event(self, event):
+        self.service.events().insert(calendarId=self.calendar_id, body=event).execute()
+
 
 def main():
     file_path = os.getenv('CREDENTIALS_FILEPATH')
     if file_path is None:
-        print('Credentials file not provided')
-        return
+        raise ValueError('Credentials file not provided')
     if not os.path.isfile(file_path):
-        print('Credentials file not found')
-        return
+        raise FileNotFoundError('Credentials file not found')
 
-    calendar = GoogleCalendar(credentials_filename=file_path)
-    calendar_list = calendar.service.calendarList().list().execute()
-    print(calendar_list)
+    calendar_id = os.getenv('CALENDAR_ID')
+    if calendar_id is None:
+        raise ValueError('Calendar ID not provided')
+
+    calendar = GoogleCalendar(credentials_filename=file_path, calendar_id=calendar_id)
 
 if __name__ == '__main__':
     main()
