5afa09bd7a6702540157e59b09460840ba1dbfd9
aaaa24 <chernov.arseniy@yandex.ru>
2026-09-02 23:42:55 +0300

    add get_weeks_schedule

--- main.py
+++ main.py
@@ -30,57 +30,65 @@ class Schedule:
 
     def get_week_schedule(self, date=None):
         url = f'{self.BASE_URL}/{self.schedule_id}'
-        response = requests.get(url, params={'date': date})
+        date_str = date.strftime('%Y-%m-%d') if date else None
+        response = requests.get(url, params={'date': date_str})
         response.raise_for_status()
         return response.json()
 
+    def get_weeks_schedule(self, weeks_count=None):
+        weeks_schedule = []
+        for i in range(weeks_count):
+            date = datetime.today() + timedelta(weeks=i)
+            weeks_schedule.append(self.get_week_schedule(date))
+        return weeks_schedule
 
-def create_iso8601(date_str, time_str):
-    dt_local = datetime.strptime(f'{date_str} {time_str}', '%Y-%m-%d %H:%M')
-    utc3 = timezone(timedelta(hours=3))
-    dt_local = dt_local.replace(tzinfo=utc3)
-    dt_utc = dt_local.astimezone(timezone.utc)
-    result = dt_utc.strftime('%Y%m%dT%H%M%S') + 'Z'
-    return result
+    @staticmethod
+    def create_iso8601(date_str, time_str):
+        dt_local = datetime.strptime(f'{date_str} {time_str}', '%Y-%m-%d %H:%M')
+        utc3 = timezone(timedelta(hours=3))
+        dt_local = dt_local.replace(tzinfo=utc3)
+        dt_utc = dt_local.astimezone(timezone.utc)
+        result = dt_utc.strftime('%Y%m%dT%H%M%S') + 'Z'
+        return result
 
+    @staticmethod
+    def create_description(lesson):
+        rename_type_names = {
+            'Лекции': 'Лекция'
+        }
 
-def create_description(lesson):
-    rename_type_names = {
-        'Лекции': 'Лекция'
-    }
+        description_lines = []
+        if lesson['typeObj']:
+            description_lines.append(rename_type_names.get(lesson['typeObj']['name']) or lesson['typeObj']['name'])
+        if lesson['groups']:
+            description_lines.append(f'Группы: {', '.join(sorted([group['name'] for group in lesson['groups']]))}')
+        if lesson['teachers']:
+            description_lines.append(', '.join([teacher['full_name'] for teacher in lesson['teachers']]))
+        if lesson['lms_url']:
+            description_lines.append(f'<a href={lesson['lms_url']}>СДО</a>')
+        description = '\n'.join(description_lines)
+        return description
 
-    description_lines = []
-    if lesson['typeObj']:
-        description_lines.append(rename_type_names.get(lesson['typeObj']['name']) or lesson['typeObj']['name'])
-    if lesson['groups']:
-        description_lines.append(f'Группы: {', '.join(sorted([group['name'] for group in lesson['groups']]))}')
-    if lesson['teachers']:
-        description_lines.append(', '.join([teacher['full_name'] for teacher in lesson['teachers']]))
-    if lesson['lms_url']:
-        description_lines.append(f'<a href={lesson['lms_url']}>СДО</a>')
-    description = '\n'.join(description_lines)
-    return description
-
-
-def create_events_from_week_schedule(week_schedule):
-    events = []
-    for day in week_schedule['days']:
-        for lesson in day['lessons']:
-            event = {
-                'summary': lesson['subject'],
-                'description': create_description(lesson),
-                'location': ';'.join(
-                    [', '.join((auditory['building']['abbr'], auditory['name'])) for auditory in lesson['auditories']]
-                ),
-                'start': {
-                    'dateTime': create_iso8601(day['date'], lesson['time_start']),
-                },
-                'end': {
-                    'dateTime': create_iso8601(day['date'], lesson['time_end']),
-                },
-            }
-            events.append(event)
-    return events
+    @staticmethod
+    def create_events_from_week_schedule(week_schedule):
+        events = []
+        for day in week_schedule['days']:
+            for lesson in day['lessons']:
+                event = {
+                    'summary': lesson['subject'],
+                    'description': Schedule.create_description(lesson),
+                    'location': ';'.join(
+                        [', '.join((auditory['building']['abbr'], auditory['name'])) for auditory in lesson['auditories']]
+                    ),
+                    'start': {
+                        'dateTime': Schedule.create_iso8601(day['date'], lesson['time_start']),
+                    },
+                    'end': {
+                        'dateTime': Schedule.create_iso8601(day['date'], lesson['time_end']),
+                    },
+                }
+                events.append(event)
+        return events
 
 
 def main():
