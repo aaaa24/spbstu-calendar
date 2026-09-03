b6c58b831bdc329b8253fa91b6cc842187f94567
aaaa24 <chernov.arseniy@yandex.ru>
2026-09-03 23:21:55 +0300

    add GoogleCalendar.get_events_list, update_calendar, compare_events

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from datetime import timedelta, timezone, datetime
+from datetime import timedelta, datetime
 
 import dotenv
 import requests
@@ -19,8 +19,29 @@ class GoogleCalendar:
         self.calendar_id = calendar_id
 
     def insert_event(self, event):
-        print(event)
         self.service.events().insert(calendarId=self.calendar_id, body=event).execute()
+
+    def get_events_list(self, *, time_min=None, time_max=None):
+        events = []
+
+        time_min_str = time_min and Schedule.datetime_to_str(time_min)
+        time_max_str = time_max and Schedule.datetime_to_str(time_max)
+
+        response = self.service.events().list(
+            calendarId=self.calendar_id,
+            timeMin=time_min_str, timeMax=time_max_str
+        ).execute()
+        events.extend(response['items'])
+
+        while response.get('nextPageToken'):
+            response = self.service.events().list(
+                calendarId=self.calendar_id,
+                timeMin=time_min_str, timeMax=time_max_str,
+                pageToken=response.get['nextPageToken']
+            ).execute()
+            events.extend(response['items'])
+
+        return events
 
 
 class Schedule:
@@ -36,7 +57,7 @@ class Schedule:
         response.raise_for_status()
         return response.json()
 
-    def get_weeks_schedule(self, weeks_count=None):
+    def get_weeks_schedule(self, weeks_count=1):
         weeks_schedule = []
         for i in range(weeks_count):
             date = datetime.today() + timedelta(weeks=i)
@@ -44,9 +65,13 @@ class Schedule:
         return weeks_schedule
 
     @staticmethod
-    def create_iso8601(date_str, time_str):
+    def datetime_to_str(dt):
+        return dt.strftime('%Y-%m-%dT%H:%M:%S') + '+03:00'
+
+    @staticmethod
+    def create_str_iso(date_str, time_str):
         dt_local = datetime.strptime(f'{date_str} {time_str}', '%Y-%m-%d %H:%M')
-        result = dt_local.strftime('%Y-%m-%dT%H:%M:%S') + '+03:00'
+        result = Schedule.datetime_to_str(dt_local)
         return result
 
     @staticmethod
@@ -79,20 +104,55 @@ class Schedule:
                         [', '.join((auditory['building']['abbr'], auditory['name'])) for auditory in lesson['auditories']]
                     ),
                     'start': {
-                        'dateTime': Schedule.create_iso8601(day['date'], lesson['time_start']),
+                        'dateTime': Schedule.create_str_iso(day['date'], lesson['time_start']),
                     },
                     'end': {
-                        'dateTime': Schedule.create_iso8601(day['date'], lesson['time_end']),
+                        'dateTime': Schedule.create_str_iso(day['date'], lesson['time_end']),
                     },
                 }
                 events.append(event)
         return events
 
 
-def insert_event(calendar, schedule):
-    week_schedule = schedule.get_week_schedule()
-    event = schedule.create_events_from_week_schedule(week_schedule)[0]
-    calendar.insert_event(event)
+def update_calendar(calendar, schedule, weeks_count=1):
+    weeks_schedule = schedule.get_weeks_schedule(weeks_count)
+
+    schedule_events = []
+    for week_schedule in weeks_schedule:
+        schedule_events.extend(schedule.create_events_from_week_schedule(week_schedule))
+
+    now = datetime.now()
+    time_min = (now - timedelta(days=now.weekday())).replace(hour=0, minute=0, second=0, microsecond=0)
+    calendar_events = calendar.get_events_list(time_min=time_min)
+
+    comparison = compare_events(calendar_events, schedule_events)
+    print(comparison)
+
+
+def compare_events(calendar_events, schedule_events):
+    calendar_events = calendar_events.copy()
+    schedule_events = schedule_events.copy()
+    i = 0
+    while i < len(schedule_events):
+        j = 0
+        while j < len(calendar_events):
+            formatted_calendar_event = {
+                'summary': calendar_events[j]['summary'],
+                'description': calendar_events[j]['description'],
+                'location': calendar_events[j]['location'],
+                'start': {'dateTime': calendar_events[j]['start'].get('dateTime')},
+                'end': {'dateTime': calendar_events[j]['end'].get('dateTime')}
+            }
+            if schedule_events[i] == formatted_calendar_event:
+                del schedule_events[i]
+                del calendar_events[j]
+                break
+            else:
+                j += 1
+        else:
+            i += 1
+
+    return {'delete': [calendar_event['id'] for calendar_event in calendar_events], 'add': schedule_events}
 
 
 def main():
@@ -114,7 +174,8 @@ def main():
 
     schedule = Schedule(schedule_id=schedule_id)
 
-    insert_event(calendar, schedule)
+    update_calendar(calendar, schedule)
+
 
 if __name__ == '__main__':
     main()
